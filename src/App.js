997df4d6ffa34bbc7102fa8a80b6a51b96e64ed0
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-18 22:52:34 +0300

    add animate

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div className='border border-purple-500 rounded-lg m-8 h-32'>
    <h1 className='text-xl font-bold text-center text-gray-900'>Hello</h1>
     <button className='mt-10 bg-purple-500 bg-opacity-60 block mx-auto py-2 px-16
-    rounded-xl hover:bg-opacity-100 transition delay-300 easy-in-out'>
+    rounded-xl animate-bounce'>
         Login
     </button>
     </div>
