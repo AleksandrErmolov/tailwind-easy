c9b60cc9165c8ac5599c8557d23ec800ce07e144
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-18 22:51:16 +0300

    add base style transition

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className='border border-purple-500 rounded-lg m-8 h-32'>
+   <h1 className='text-xl font-bold text-center text-gray-900'>Hello</h1>
+    <button className='mt-10 bg-purple-500 bg-opacity-60 block mx-auto py-2 px-16
+    rounded-xl hover:bg-opacity-100 transition delay-300 easy-in-out'>
+        Login
+    </button>
+    </div>
+  );
+}
+
+export default App;
