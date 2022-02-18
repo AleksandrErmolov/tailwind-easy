1410168fd0db88cb1cef91995288e37346a42abd
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-18 23:25:55 +0300

    add scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import styles from './App.module.scss'
+
 
 function App() {
   return (
-    <div className='border border-purple-500 rounded-lg m-8 h-32'>
-   <h1 className='text-xl font-bold text-center text-gray-900'>Hello</h1>
-    <button className='mt-10 bg-purple-500 bg-opacity-60 block mx-auto py-2 px-16
-    rounded-xl animate-bounce dark:bg-red-600'>
+    <div className={styles.parent}>
+   <h1 className={styles.heading}>Hello</h1>
+    <button className={styles.button}>
         Login
     </button>
     </div>
