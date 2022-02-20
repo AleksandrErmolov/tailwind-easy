5d47d85862ee4f49fd3b331b4a8869e089151ae4
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-02-20 19:23:15 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import styles from './App.module.scss'
+import {useState} from "react";
 
 
 function App() {
-  return (
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    return (
     <div className={styles.parent}>
    <h1 className={styles.heading}>Hello</h1>
+        <input placeholder={'email'} value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type={'password'} placeholder={'Password'} value={password} onChange={(e) => setPassword(e.target.value)}/>
     <button className={styles.button}>
         Login
     </button>
